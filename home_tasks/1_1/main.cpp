003b747d02795e7031b85b50941ed98b1a106322
Некрасов Лев <coding1liki@gmail.com>
2024-01-11 00:10:26 +0300

    add makefile and .gitignore

--- home_tasks/1_1/main.cpp
+++ home_tasks/1_1/main.cpp
@@ -1,8 +1,57 @@
 #include <iostream>
+#include <set>
 
 using namespace std;
 
-int main () {
+void readSet(set<int_fast16_t> *set) {
+    int_fast16_t inputInt;
 
-  return 0;
+    cin >> inputInt;
+
+    while (inputInt != 0) {
+        set->insert(inputInt);
+        cin >> inputInt;
+    }
+}
+
+void readAndCulculateSymetricSubSet(set<int_fast16_t> *alreadyReadSet) {
+    int_fast16_t inputInt;
+
+    cin >> inputInt;
+
+    while (inputInt != 0) {
+        if (alreadyReadSet->contains(inputInt)) {
+            alreadyReadSet->erase(inputInt);
+        } else {
+            alreadyReadSet->insert(inputInt);
+        }
+        cin >> inputInt;
+    }
+}
+
+void writeSet(set<int_fast16_t> *set) {
+    if (set->empty()) {
+        cout << 0;
+        return;
+    }
+
+    for (auto iterator = set->begin(); iterator != set->end(); ++iterator) {
+        cout << *iterator;
+        if (iterator != set->end()) {
+            cout << " ";
+        }
+    }
+}
+
+int main() {
+    set<int_fast16_t> a, b, c;
+
+    readSet(&a);
+    readAndCulculateSymetricSubSet(&a);
+    writeSet(&a);
+    // readSet(&b);
+    //
+    // calulateSimetricSum(&a, &b, &c);
+    //
+    // writeSet(&c);
 }
