cd3af61b9df2ab930cdee658df351ddfd38644cd
Некрасов Лев <coding1liki@gmail.com>
2024-01-11 18:44:24 +0300

    add 1_3 task and reformat a bit

--- home_tasks/1_2/main.cpp
+++ home_tasks/1_2/main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
-int main () {
-  return 0;
+void readMap(unordered_map<int, int> *map, const int length) {
+    for (int i = 0; i < length; i++) {
+        int input;
+        cin >> input;
+        map->find(input) != map->end() ? (*map)[input]++ : (*map)[input] = 1;
+    }
+}
+
+
+int readMapAndCountExistedElements(unordered_map<int, int> *map, const int length) {
+    int count = 0;
+    for (int i = 0; i < length; i++) {
+        int input;
+        cin >> input;
+        map->find(input) != map->end() && (*map)[input] > 0 ? ++count && (*map)[input]-- : 0;
+    }
+
+    return count;
+}
+
+int main() {
+    unordered_map<int, int> a;
+    int n1, n2;
+    cin >> n1 >> n2;
+    readMap(&a, n1);
+
+    cout << readMapAndCountExistedElements(&a, n2);
+
+    return 0;
 }
---
+++ home_tasks/1_3/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    uint32_t degree;
+    uint16_t calculatedValuesCount;
+    uint32_t mod;
+
+    cin >> degree >> calculatedValuesCount >> mod;
+
+    return 0;
+}
